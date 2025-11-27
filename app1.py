32ec113bf6e3439a57b6998a69902ecde3d784a4
Christina33810 <luckyaroundme123@gmail.com>
2025-11-27 13:46:28 +0800

    Fix formatting of number input parameters

--- app1.py
+++ app1.py
@@ -6,7 +6,7 @@ st.header('Manager Input Section')
 st.write("Please enter the monthly sales target and select the region.")
 
 st.number_input("Enter your monthly sales target (in USD):",
-                      min_value=0,
-                      max_value=100000,
-                      value=500000)
+                min_value=0,
+                max_value=100000,
+                value=500000)
 
