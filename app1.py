af239ef09e431a77cbc0e47597c6db76b2b18c73
Christina33810 <luckyaroundme123@gmail.com>
2025-11-27 13:47:39 +0800

    Update sales target input range to 1,000,000

--- app1.py
+++ app1.py
@@ -5,8 +5,8 @@ st.title("Retail Business Dashboard")
 st.header('Manager Input Section')
 st.write("Please enter the monthly sales target and select the region.")
 
-st.number_input("Enter your monthly sales target (in USD):",
-                min_value=0,
-                max_value=100000,
-                value=500000)
-
+monthly_sales_target = st.number_input("Enter your monthly sales target (in USD):",
+                                      min_value=0,
+                                      max_value=1000000,
+                                      value=500000)
+                        
