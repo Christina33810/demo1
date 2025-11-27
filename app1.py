287ffc6fc979389f3575ce051a5e970252c71f4b
Christina33810 <luckyaroundme123@gmail.com>
2025-11-27 13:43:14 +0800

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Retail Business Dashboard")
+
+st.header('Manager Input Section')
+st.write("Please enter the monthly sales target and select the region.")
+
+st.
